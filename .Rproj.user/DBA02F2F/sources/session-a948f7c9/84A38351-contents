de3d66e9e3951ed5906398466936c6e239a410bf
D-Zacster <zacb310@gmail.com>
2025-04-15 17:10:46 -0500

    Updated load_libraries

--- .Rproj.user/DBA02F2F/sources/session-a948f7c9/84A38351-contents
+++ .Rproj.user/DBA02F2F/sources/session-a948f7c9/84A38351-contents
@@ -3,3 +3,10 @@
 # install("./HPRC") while in the project will install what is currently written
 
 # devtools::install_github('D-Zacster/HPRC') will install from github
+
+# The following lines in git will add changes, commit them, select the desired
+# branch (optional), then push the commit.
+# git add .
+# git commit -m "Some commit message"
+# git branch -M main
+# git push -u origin main
--- R/load_libraries.R
+++ R/load_libraries.R
@@ -2,15 +2,20 @@
 #'
 #' This function loads a list of libraries. If any of the libraries are missing, it installs them first.
 #' @param libraries A vector of library names as strings. Defaults to "tidyverse","magrittr","haven", and "ggplot2"
+#' @param update TRUE/FALSE. Indicates whether you want all the packages to be updated. Defaults to FALSE.
 #' @keywords library
 #' @export
 #' @examples
 #' load_libraries()
-load_libraries = function(libraries=c("tidyverse","magrittr","haven","ggplot2")) {
-  missing_packages = setdiff(libraries, rownames(installed.packages()))
-  if(length(missing_packages)>0) {
-    print(paste0("Missing the following package(s): ",paste(missing_packages,collapse = ", "),". Installing missing package(s)!"))
-    install.packages(missing_packages)
+load_libraries = function(libraries=c("tidyverse","magrittr","haven","ggplot2"),update=F) {
+  if(update==TRUE) {
+    packages_to_be_installed = libraries
+  } else {
+    packages_to_be_installed = setdiff(libraries, rownames(installed.packages()))
+  }
+  if(length(packages_to_be_installed)>0) {
+    print(paste0("Installing the following package(s): ",paste(packages_to_be_installed,collapse = ", "),"."))
+    install.packages(packages_to_be_installed)
   }
   sapply(libraries,library,character = T)
   print(paste0("The following packages were loaded: ",paste(libraries,collapse = ", "),"."))
