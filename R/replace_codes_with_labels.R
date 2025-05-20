b57239545c257f575e439c64fa277f5f1f00f5d2
D-Zacster <zacb310@gmail.com>
2025-05-20 10:46:37 -0500

    Bug fix for replace_codes_with_labels to account for NAs

--- R/replace_codes_with_labels.R
+++ R/replace_codes_with_labels.R
@@ -5,17 +5,22 @@
 #' @param label_string String fed into the `which` argument of the attr() function. Defaults to "labels".
 #' @export
 #' @examples
-#' haven_loaded_data_frame %>% 
+#' haven_loaded_data_frame %>%
 #'   mutate(new_column = replace_codes_with_labels(haven_labelled_vector))
 replace_codes_with_labels = function(labelled_vector, label_string="labels") {
   vector_values_and_labels = attr(labelled_vector,label_string)
-  label_table = data.frame(Code = unname(vector_values_and_labels), 
+  label_table = data.frame(Code = unname(vector_values_and_labels),
                            Value = names(vector_values_and_labels))
-  
+
   new_vector = c()
   for (i in 1:length(labelled_vector)) {
+    if (is.na(labelled_vector[i])) {
+      new_element = NA
+    } else {
+      new_element = label_table$Value[label_table$Code==labelled_vector[i]]
+    }
     new_vector = c(new_vector,
-                   label_table$Value[label_table$Code==labelled_vector[i]])
+                   new_element)
   }
   new_vector
 }
