a35ab62de96fdf01ced2162774b5bd450b163265
D-Zacster <zacb310@gmail.com>
2025-10-29 11:47:33 -0500

    Updated extract_column_labels to return a whole data.frame rather than a list

--- R/extract_column_labels.R
+++ R/extract_column_labels.R
@@ -1,12 +1,13 @@
 #' Extract labels for each column in a data frame
 #'
-#' This function returns a list of the labels for all columns in a dataframe.
+#' This function returns a data.frame() object of the names and labels for all columns in a dataframe.
 #' @param input_df A data.frame() object.
 #' @param which_string String fed into the `which` argument of the attr() function. Defaults to "label".
 #' @export
 extract_column_labels = function(input_df,which_string="label") {
   label_list = c()
-  for (variable in names(input_df)) {
+  variables = names(input_df)
+  for (variable in variables) {
     label_str = unname(attr(input_df[[variable]],which_string))
     if (is.null(label_str)) {
       label = NA
@@ -15,5 +16,6 @@ extract_column_labels = function(input_df,which_string="label") {
     }
     label_list = c(label_list,label)
   }
-  label_list
+  label_table = data.frame(Variable = variables, Label = label_list)
+  label_table
 }
